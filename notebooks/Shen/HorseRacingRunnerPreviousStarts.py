2fb2b5901c6049a20f390269081e98895f415156
Shen Zhang <zhangshen20@hotmail.com>
2020-10-11 17:10:29 +1100

    Add optimize on 'RunnerPreviousStartsUnique'

--- notebooks/Shen/HorseRacingRunnerPreviousStarts.py
+++ notebooks/Shen/HorseRacingRunnerPreviousStarts.py
@@ -11,6 +11,12 @@
 
 # COMMAND ----------
 
+from pyspark.sql.functions import udf, col, from_unixtime, from_utc_timestamp, from_json
+from pyspark.sql.types import StringType, StructField, StructType
+import json, time, requests
+
+# COMMAND ----------
+
 # SOURCE SETTING 
 SourceDataSetName = "RunnerStarts"
 SourceDataPathBase = "/mnt/gamble/DELTA/SILVER/DATA"
@@ -27,6 +33,15 @@ SilverDataPath = "%s/%s" % (SilverDataPathBase, DataSetName)
 SilverCheckPointPath = "%s/%s" % (SilverCheckPointPathBase, DataSetName)
 SilverTableName = 'Silver' + DataSetName
 
+# ----------------------------------------------------------
+
+DataSetNameUnique = "RunnerPreviousStartsUnique"
+
+# SILVER SETTING
+SilverDataPathUnique = "%s/%s" % (SilverDataPathBase, DataSetNameUnique)
+SilverCheckPointPathUnique = "%s/%s" % (SilverCheckPointPathBase, DataSetNameUnique)
+SilverTableNameUnique = 'Silver' + DataSetNameUnique
+
 
 # COMMAND ----------
 
@@ -41,7 +56,7 @@ SilverTableName = 'Silver' + DataSetName
 # from pyspark.sql.functions import input_file_name
 from pyspark.sql.functions import *
 
-runnerPreviousStartsDF = (
+(
 (spark.readStream
   .format("delta")
   .load(SourceDataPath) 
@@ -81,14 +96,71 @@ runnerPreviousStartsDF = (
   "runner_previousStarts.trackCondition",
   "runner_previousStarts.time",
   "runner_previousStarts.stewardsComment"
-  )
+).writeStream
+ .trigger(once=True)
+ .format("delta")
+ .outputMode("append")
+ .option("checkpointLocation", "%s" % SilverCheckPointPath)
+ .start("%s" % SilverDataPath)
 )
 
 # COMMAND ----------
 
-(runnerPreviousStartsDF.writeStream
-  .trigger(once=True)
+# (runnerPreviousStartsDF.writeStream
+#   .trigger(once=True)
+#   .format("delta")
+#   .outputMode("append")
+#   .option("checkpointLocation", "%s" % SilverCheckPointPath)
+#   .start("%s" % SilverDataPath))
+
+# COMMAND ----------
+
+while spark.streams.active != []:
+  print("Waiting for streaming query to finish.")
+  time.sleep(5)
+
+# COMMAND ----------
+
+spark.sql(""" OPTIMIZE delta.`%s` """ % SilverDataPath)
+
+# COMMAND ----------
+
+(spark.readStream
   .format("delta")
-  .outputMode("append")
-  .option("checkpointLocation", "%s" % SilverCheckPointPath)
-  .start("%s" % SilverDataPath))
+  .load(SilverDataPath)
+  .createOrReplaceTempView("runner_previous_starts_temp"))
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC CREATE OR REPLACE TEMP VIEW runner_previous_starts_unique_temp AS (
+# MAGIC   SELECT venueAbbreviation, startDate, raceNumber, runnerName, last(prizeMoney) as prizeMoney, last(last20Starts) as last20Starts, last(startType) as startType,
+# MAGIC          last(finishingPosition) as finishingPosition, last(numberOfStarters) as numberOfStarters, last(draw) as draw, last(margin) as margin, last(audio) as audio,
+# MAGIC          last(previewVideo) as previewVideo, last(Video) as Video, min(distance) as distance, last(class) as class, last(handicap) as handicap, last(rider) as rider,
+# MAGIC          last(startingPosition) as startingPosition, last(odds) as odds, last(winnerOrSecond) as winnerOrSecond, last(positionInRun) as positionInRun,
+# MAGIC          last(trackCondition) as trackCondition, last(time) as time, last(stewardsComment) as stewardsComment
+# MAGIC   FROM runner_previous_starts_temp
+# MAGIC   GROUP BY venueAbbreviation, startDate, raceNumber, runnerName
+# MAGIC )
+
+# COMMAND ----------
+
+(spark.table("runner_previous_starts_unique_temp")
+  .writeStream
+  .format("delta")
+  .outputMode("complete")
+  .option("checkpointLocation", SilverCheckPointPathUnique)
+  .trigger(once=True)
+  .start(SilverDataPathUnique)
+)
+
+# COMMAND ----------
+
+while spark.streams.active != []:
+  print("Waiting for streaming query to finish.")
+  time.sleep(5)
+
+# COMMAND ----------
+
+spark.sql(""" OPTIMIZE delta.`%s` """ % SilverDataPathUnique)
