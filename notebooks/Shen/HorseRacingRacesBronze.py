96b593011aec953a7e6949f2673b8a860af0e43d
Shen Zhang <zhangshen20@hotmail.com>
2020-10-11 18:19:12 +1100

    Add 'pyspark' functions

--- notebooks/Shen/HorseRacingRacesBronze.py
+++ notebooks/Shen/HorseRacingRacesBronze.py
@@ -11,6 +11,12 @@
 
 # COMMAND ----------
 
+from pyspark.sql.functions import udf, col, from_unixtime, from_utc_timestamp, from_json
+from pyspark.sql.types import StringType, StructField, StructType
+import json, time, requests
+
+# COMMAND ----------
+
 MOUNT_NAME = "gamble"
 MOUNT_PATH = "/mnt/%s" % MOUNT_NAME
 
