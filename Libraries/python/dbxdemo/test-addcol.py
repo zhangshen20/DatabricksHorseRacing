89b004637495a44bae78dfc3e54f491fee1c44a3
zhangshen20 <zhangshen20@hotmail.com>
2021-02-26 22:19:32 +1100

    Add Test Library

---
+++ Libraries/python/dbxdemo/test-addcol.py
@@ -0,0 +1,33 @@
+# Databricks notebook source
+# test-addcol.py
+import pytest
+
+
+from dbxdemo.spark import get_spark
+from dbxdemo.appendcol import with_status
+
+
+class TestAppendCol(object):
+
+  def test_with_status(self):
+    source_data = [
+        ("paula", "white", "paula.white@example.com"),
+        ("john", "baer", "john.baer@example.com")
+    ]
+    source_df = get_spark().createDataFrame(
+        source_data,
+        ["first_name", "last_name", "email"]
+    )
+
+    actual_df = with_status(source_df)
+
+    expected_data = [
+        ("paula", "white", "paula.white@example.com", "checked"),
+        ("john", "baer", "john.baer@example.com", "checked")
+    ]
+    expected_df = get_spark().createDataFrame(
+        expected_data,
+        ["first_name", "last_name", "email", "status"]
+    )
+
+    assert(expected_df.collect() == actual_df.collect())
