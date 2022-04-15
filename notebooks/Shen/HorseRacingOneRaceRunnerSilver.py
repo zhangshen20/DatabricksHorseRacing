a00de9a9634b4c1d9207c69c55823bae33d0106b
Shen Zhang <zhangshen20@hotmail.com>
2022-04-15 17:32:05 +1000

    Re-Run

--- notebooks/Shen/HorseRacingOneRaceRunnerSilver.py
+++ notebooks/Shen/HorseRacingOneRaceRunnerSilver.py
@@ -114,10 +114,12 @@ oneRaceRunnerDF = (
 
 # COMMAND ----------
 
+import json, time, requests
+
 while spark.streams.active != []:
   print("Waiting for streaming '%s' to finish." % SilverDataPath)
   time.sleep(5)
 
 # COMMAND ----------
 
-spark.sql(""" OPTIMIZE delta.`%s` """ % SilverDataPath)
+# spark.sql(""" OPTIMIZE delta.`%s` """ % SilverDataPath)
